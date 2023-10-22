4e929dd14d1b587cffddca1e5f0167051514f8dc
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2023-10-22 16:26:45 -0600

    jack

---
+++ (1.3)-jack/jack.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int int1;
+    int int2;
+    int int3;
+
+    scanf("%d", &int1);
+    scanf("%d", &int2);
+    scanf("%d", &int3);
+
+    printf("%d", int1 * int2 * int3);
+
+    return 0;
+}
