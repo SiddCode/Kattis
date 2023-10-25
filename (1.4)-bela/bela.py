cc71c2663cb14f3f09478625e3c71ef9bec09e5d
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2023-10-24 19:27:54 -0600

    bela

--- (1.4)-bela/bela.py
+++ (1.4)-bela/bela.py
@@ -1,15 +1,26 @@
 u = input()
-sum = 0
 
+int_input = ''
+
+
+for i in range(len(u)):
+    if u[i] != ' ':
+        int_input += u[i]
+    else:
+         break
+
+int_input = int(int_input)
+sum = 0
 
 dominant = {'A': 11, 'K': 4, 'Q': 3, 'J': 20, 'T': 10, '9': 14, '8': 0, '7': 0}
 
 non_dominant = {'A': 11, 'K': 4, 'Q': 3, 'J': 2, 'T': 10, '9': 0, '8': 0, '7': 0}
 
 
-for i in range(int(u[0]) * 4):
+for i in range(int_input * 4):
     card_input = input()
-    if card_input[1] == u[2]:
+
+    if card_input[1] == u[-1]:
         sum += dominant[card_input[0]]
     else:
         sum += non_dominant[card_input[0]]
