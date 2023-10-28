d3bb9053d393740ac0e5e39cb17279776b54741f
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2023-10-28 01:27:29 -0600

    greater

---
+++ (1.3)-greater/greater.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int int1, int2;
+
+    scanf("%d %d", &int1, &int2);
+
+    if (int1 > int2) {
+        printf("1");
+    } else
+    {
+        printf("0");
+    }
+    
+
+    return 0;
+}
