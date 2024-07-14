514167830d6185ce5a911f2c000e2492dce51d4a
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2024-07-14 17:33:08 -0600

    commit

--- (1.4)-Reversed Binary Numbers/Reversed Binary Numbers.py
+++ (1.4)-Reversed Binary Numbers/Reversed Binary Numbers.py
@@ -2,12 +2,12 @@ p = str(bin(int(input())))
 a = ''
 c = 0
 
+
 for i in range(len(p) - 1, 1, -1):
     a += p[i]
 
 for i in range(len(a)-1, -1, -1):
-    c += (2^(len(a)-i)) * int(a[i])
-    print(len(a) - i)
+    c += 2**(len(a)- i - 1) * int(a[i])
     
 print(c)
 
